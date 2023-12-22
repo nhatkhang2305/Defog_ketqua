1811e9a01fd6e79db30b0ee508b3803c4f40bccb
nhatkhang2305 <150947790+nhatkhang2305@users.noreply.github.com>
2023-12-22 18:08:37 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include "hazeremoval.h"
+#include "/usr/local/include/opencv4/opencv2/opencv.hpp"
+#include </usr/local/include/opencv4/opencv2/core/cuda.hpp>
+#include <iostream>
+#include <ctime>
+
+using namespace cv;
+using namespace std;
+int main() {
+    cout<<"hello";
+    int num_iterations = 100;
+    double total_cpu_time = 0.0;
+
+    for (int i = 0; i < num_iterations; i++) {
+        clock_t start, end;
+        double cpu_time_used;
+        start = clock();
+
+        const char* img_path = "suong1.jpg";
+        Mat in_img = imread(img_path);
+        
+
+        Mat out_img(in_img.rows, in_img.cols, CV_8UC3);
+        unsigned char* indata = in_img.data;
+        unsigned char* outdata = out_img.data;
+
+        CHazeRemoval hr;
+        cout << hr.InitProc(in_img.cols, in_img.rows, in_img.channels()) << endl;
+        cout << hr.Process(indata, outdata, in_img.cols, in_img.rows, in_img.channels()) << endl;
+
+
+        cv::imwrite("F:/photo/ketqua1/khu" + to_string(i + 1) + ".jpg", out_img);
+
+        end = clock();
+        cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+        total_cpu_time += cpu_time_used;
+
+        cout << "Iteration " << (i + 1) << " CPU time: " << cpu_time_used << " s" << endl;
+    }
+
+    double average_cpu_time = total_cpu_time / num_iterations;
+    cout << "Average CPU time: " << average_cpu_time << " s" << endl;
+
+    return 0;
+}
